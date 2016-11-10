491491d34d917cb8fc1cf0b61fcf9815f18606c6
evtuhovaleksander <evtuhov.aleksander@gmail.com>
2016-11-11 00:16:16 +0300

    com5

--- src/interface_test/start_class.java
+++ src/interface_test/start_class.java
@@ -1,5 +1,7 @@
 package interface_test;
 
+import system_class_package.SQL_Class;
+
 import java.io.IOException;
 
 import javax.swing.JFrame;
@@ -9,6 +11,7 @@ public class start_class {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.println("xui");
+		System.out.println(SQL_Class.readInt32("select count(*) from testt"));
 		TestForm window = new TestForm();
 		window.frame.setVisible(true);
 	}
--- src/system_class_package/SQL_Class.java
+++ src/system_class_package/SQL_Class.java
@@ -1,8 +1,57 @@
 package system_class_package;
 
-/**
- * Created by Allex Dark on 10.11.2016.
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Objects;
+
 public class SQL_Class {
 
-}
+        private   String db="testpr";
+        private   String url = "jdbc:mysql://localhost:3306/";
+        private   String user = "tu";
+        private   String password = "QAz1234567890";
+
+
+        private  Connection con;
+        private  Statement stmt;
+        private  ResultSet rs;
+
+    public SQL_Class() {
+        url+=db;
+    }
+
+    public void closeConnections(){
+            try { con.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { rs.close(); } catch(SQLException se) { /*can't do anything */ }
+        }
+        public static Object readObject(String query){
+            SQL_Class cl = new SQL_Class();
+            try {
+
+
+                cl.con = DriverManager.getConnection(cl.url, cl.user, cl.password);
+                // getting Statement object to execute query
+                cl.stmt = cl.con.createStatement();
+
+                // executing SELECT query
+                cl.rs = cl.stmt.executeQuery(query);
+                if(cl.rs.next())return cl.rs.getObject(0);else return null;
+
+            } catch (SQLException sqlEx) {
+                sqlEx.printStackTrace();
+            } finally {
+                cl.closeConnections();
+                return null;
+            }
+        }
+
+        public static int readInt32(String query){
+            Object res=readObject(query);
+            if(res!=null)return (int)res;else return 0;
+        }
+
+    }
